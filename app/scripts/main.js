ce9696a8797dbf127fe1afef3b955bf916da91bf
jiwang <jiwang@alibaba-inc.com>
2015-05-11 23:24:26 +0800

    init

---
+++ app/scripts/main.js
@@ -0,0 +1,7 @@
+//define(["jquery", "jquery.alpha", "jquery.beta"], function($) {
+define(["jquery"], function($) {
+    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
+    $(function() {
+      console.log($);
+    });
+});
---
+++ app/scripts/requirejs-config.js
@@ -0,0 +1,15 @@
+// Place third party dependencies in the lib folder
+//
+// Configure loading modules from the lib directory,
+// except 'app' ones, 
+var requirejs = {
+    "baseUrl": "scripts",
+    "paths": {
+        'jquery': '../../bower_components/jquery/dist/jquery'
+    },
+    "shim": {
+    }
+};
+
+// Load the main app module to start the app
+//requirejs(["main"]);
