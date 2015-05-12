24fe881348fe317cb8b8576eb8e4bbf9239eb506
jiwang <jiwang@alibaba-inc.com>
2015-05-12 12:56:46 +0800

    put requirejs-config into data-main of requirejs

--- app/scripts/requirejs-config.js
+++ app/scripts/requirejs-config.js
@@ -2,14 +2,14 @@
 //
 // Configure loading modules from the lib directory,
 // except 'app' ones, 
-var requirejs = {
+requirejs.config({
     "baseUrl": "scripts",
     "paths": {
         'jquery': '../../bower_components/jquery/dist/jquery',
     },
     "shim": {
     }
-};
+});
 
 // Load the main app module to start the app
-//requirejs(["main"]);
+requirejs(["main"]);
